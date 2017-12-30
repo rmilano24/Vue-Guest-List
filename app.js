79316c19b9184af99d4ebb72a83ba0eadb8edc67
rmilano24 <rmilano24@gmail.com>
2017-12-29 16:20:30 -0800

    put related content in a group object

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 new Vue({
     el: '#app',
     data: {
-        eventDate: 'August 14th - 16th',
-        eventTitle: 'Summer Festival!',
-        eventDescription: 'Its back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+        event: {
+            eventDate: 'August 14th - 16th',
+            eventTitle: 'Summer Festival!',
+            eventDescription: 'Its back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+        }
     }
 });
