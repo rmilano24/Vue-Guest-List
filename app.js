0c966ee27967feca6dc28f489839ae06efd09f85
rmilano24 <rmilano24@gmail.com>
2017-12-30 10:22:27 -0800

    directives

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ new Vue({
         event: {
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
+            eventTitle2: 'v text output',
+            signUpText: 'Add your name to the guest list for <em>exclusive</em> offers:',
             eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
         },
         newNameText: '',
