1d070cacab1d7eb6f7c356a51bf195901e9cce77
rmilano24 <rmilano24@gmail.com>
2017-12-29 16:12:35 -0800

    add dynamic card info

---
+++ app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        eventDate: 'August 14th - 16th',
+        eventTitle: 'Summer Festival!',
+        eventDescription: 'Its back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+    }
+});
