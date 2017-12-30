6172f20257103405c2ca7df22e1c0c47e16684e1
rmilano24 <rmilano24@gmail.com>
2017-12-29 16:27:21 -0800

    escape string with \

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ new Vue({
         event: {
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
-            eventDescription: 'Its back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+            eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
         }
     }
 });
