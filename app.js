9edd6f22c5057e6d5e800e397286cf03622de401
rmilano24 <rmilano24@gmail.com>
2017-12-29 16:44:05 -0800

    use v-model for 2 way data binding and cofigure submit button to add names to list dynamically

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ new Vue({
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
             eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
+        },
+        newNameText: '',
+        guestName: []
+    },
+    methods: {
+        // formSubmitted: function() {
+        //     console.log(this.newNameText)
+        // } TEST IN LOGO FIRST BY RYAN
+        formSubmitted: function() {
+            this.guestName.push(this.newNameText)
+            this.newNameText = ''
         }
     }
 });
