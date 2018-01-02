69fd56964d23a5d153463d1c81314cf9e09669bf
rmilano24 <rmilano24@gmail.com>
2018-01-01 16:07:27 -0800

    add progress bar

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ new Vue({
             eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
         },
         newNameText: '',
-        guestName: ['James', 'Chris', 'Sam'],
-        formSubmitClass: "",
+        guestName: [],
         appStyles: {
             marginTop: '25px',
-        }
+        },
+        eventCapacity: 25,
+        eventCapacityPercentage: 0
     },
     methods: {
         // formSubmitted: function() {
         //     console.log(this.newNameText)
         // } TEST IN LOGO FIRST BY RYAN
         formSubmitted: function() {
-            if(this.newNameText.length > 0) {
+            if(this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
                 this.guestName.push(this.newNameText)
                 this.newNameText = '' 
-                this.formSubmitClass = "submitted"
+                this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100)
             }
         }
     }
