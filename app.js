cea161f1d289e03a1c8be2f098559d83d0d2b33f
rmilano24 <rmilano24@gmail.com>
2018-01-01 17:27:52 -0800

    add new vue instance

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ new Vue({
         },
         newNameText: '',
         guestName: [],
-        appStyles: {
-            marginTop: '25px',
-        },
         eventCapacity: 25,
         eventCapacityPercentage: 0
     },
@@ -47,3 +44,10 @@ new Vue({
         }
     }
 });
+
+new Vue({
+    el: '#navigation',
+    data: {
+        appName: 'Guest List'
+    }
+})
