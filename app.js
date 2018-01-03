15f77c5ff4b995597e345a7dbb6e755585772fb7
rmilano24 <rmilano24@gmail.com>
2018-01-03 13:42:30 -0800

    create tamplate for custom nav links

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ new Vue({
     data: {
         appName: 'Guest List',
         navLinks: [
-            {name: "Home", id: 1}, 
-            {name: "Upcoming Events", id: 2},
-            {name: "Guest Benefits", id: 3},
-            {name: "Latest News", id: 4}
+            {name: "Home", id: 1, url: "https://www.google.com"}, 
+            {name: "Upcoming Events", id: 2, url: "https://www.amazoncom"},
+            {name: "Guest Benefits", id: 3, url: "https://www.ebay.com"},
+            {name: "Latest News", id: 4, url: "https://www.facebook.com"}
         ]
     }
 })
