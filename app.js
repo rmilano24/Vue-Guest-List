65b48b244a33ee8870c18eb8b06919e4e0f5707c
rmilano24 <rmilano24@gmail.com>
2018-01-03 13:53:18 -0800

    make instances easier to access

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-new Vue({
+var vm1 = new Vue({
     el: '#app',
     data: {
         event: {
@@ -45,7 +45,7 @@ new Vue({
     }
 });
 
-new Vue({
+var vm2 = new Vue({
     el: '#navigation',
     data: {
         appName: 'Guest List',
@@ -55,5 +55,11 @@ new Vue({
             {name: "Guest Benefits", id: 3, url: "https://www.ebay.com"},
             {name: "Latest News", id: 4, url: "https://www.facebook.com"}
         ]
+    }, 
+    methods: {
+        capacityChanged: function() {
+            vm1.eventCapacity = "15"
+        }
     }
 })
+
