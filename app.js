c421423a06e9a6097779afb11423906e664849a8
rmilano24 <rmilano24@gmail.com>
2018-01-03 13:28:12 -0800

    loop through objects

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ new Vue({
 new Vue({
     el: '#navigation',
     data: {
-        appName: 'Guest List'
+        appName: 'Guest List',
+        navLinks: [
+            {name: "Home", id: 1}, 
+            {name: "Upcoming Events", id: 2},
+            {name: "Guest Benefits", id: 3},
+            {name: "Latest News", id: 4}
+        ]
     }
 })
